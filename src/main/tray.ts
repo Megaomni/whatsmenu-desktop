4d04e5082f8b825e0ae5683c342d91146bda1867
Bruno Santos <brsantos197@gmail.com>
2024-04-17 13:58:31 -0300

    chore: close app icon

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -7,11 +7,15 @@ import { whatsmenuWindow } from "../windows/whatsmenu-window";
 app.whenReady().then(() => {
   const tray = new Tray(nativeImage.createFromPath(path.resolve(__dirname, './app_icon.png')).resize({ width: 16, height: 16 }))
   const menu = Menu.buildFromTemplate([
-    { label: 'Encerrar', click: () => {
-      whatsmenuWindow.forceCloseWindow()
-      botWindow.forceCloseWindow()
-      app.quit()
-    } }
+    { 
+      icon: nativeImage.createFromPath(path.resolve(__dirname, './close_icon.png')).resize({ width: 16, height: 16 }),
+      label: 'Encerrar',
+      click: () => {
+        whatsmenuWindow.forceCloseWindow()
+        botWindow.forceCloseWindow()
+        app.quit()
+      }
+    }
   ])
   tray.setContextMenu(menu)
   tray.on('click', () => {
