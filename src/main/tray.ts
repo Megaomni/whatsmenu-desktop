f54446f8ae042bbc5563fd0a2e89148761638617
Bruno Santos <brsantos197@gmail.com>
2024-04-25 16:37:12 -0300

    fix: tray close app

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -1,8 +1,7 @@
 import { Menu, Tray, app, nativeImage } from "electron";
 import path from 'node:path';
 import { mainWindow } from '.';
-import { botWindow } from "../windows/bot-window";
-import { whatsmenuWindow } from "../windows/whatsmenu-window";
+import { tabsWindow } from "../windows/tabs-window";
 
 app.whenReady().then(() => {
   const tray = new Tray(nativeImage.createFromPath(path.resolve(__dirname, './app_icon.png')).resize({ width: 16, height: 16 }))
@@ -11,17 +10,14 @@ app.whenReady().then(() => {
       icon: nativeImage.createFromPath(path.resolve(__dirname, './close_icon.png')).resize({ width: 16, height: 16 }),
       label: 'Encerrar',
       click: () => {
-        whatsmenuWindow.forceCloseWindow()
-        botWindow.forceCloseWindow()
+        tabsWindow.forceCloseWindow()
         app.quit()
       }
     }
   ])
   tray.setContextMenu(menu)
   tray.on('click', () => {
-    if (!mainWindow.isVisible()) {
-      mainWindow.restore()
-      mainWindow.maximize()
-    }
+    mainWindow.restore()
+    mainWindow.maximize()
   })
 })
