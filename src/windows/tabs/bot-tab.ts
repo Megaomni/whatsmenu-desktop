eaf427883074f3d7626684acd014f6ddafdacc32
Bruno Santos <brsantos197@gmail.com>
2024-05-10 10:03:11 -0300

    chore: melhorando performance
    agora robo só será iniciado depois que a aba for completamente carregada

--- src/windows/tabs/bot-tab.ts
+++ src/windows/tabs/bot-tab.ts
@@ -24,43 +24,44 @@ export const create_bot_tab = () => {
   }
   tab.setVisible(false)
 
-  whatsAppService.initBot()
-    .then(() => {
-      whatsAppService.bot.on("ready", () => {
-        if (!whatsAppService.firstConection) {
-          tab.webContents.send("onready");
-        } else {
-          whatsAppService.events.on("ready", () => {
+  tab.webContents.on('did-finish-load', () => {
+    whatsAppService.initBot()
+      .then(() => {
+        whatsAppService.bot.on("ready", () => {
+          if (!whatsAppService.firstConection) {
             tab.webContents.send("onready");
-          });
-        }
-      });
-  
-      whatsAppService.bot.on("qr", (qr) => {
-        tab.webContents.send("onqrcode", qr);
-        tab.webContents.send("log", qr);
-      });
-  
-      whatsAppService.bot.on("loading_screen", (percent, message) => {
-        tab.webContents.send("onloading", { percent, message });
-      });
-  
-      whatsAppService.bot.on("disconnected", (reason) => {
-        tab.webContents.send("ondisconnected", reason);
-      });
-  
-      whatsAppService.bot.initialize().catch((err) => {
+          } else {
+            whatsAppService.events.on("ready", () => {
+              tab.webContents.send("onready");
+            });
+          }
+        });
+    
+        whatsAppService.bot.on("qr", (qr) => {
+          tab.webContents.send("onqrcode", qr);
+          tab.webContents.send("log", qr);
+        });
+    
+        whatsAppService.bot.on("loading_screen", (percent, message) => {
+          tab.webContents.send("onloading", { percent, message });
+        });
+    
+        whatsAppService.bot.on("disconnected", (reason) => {
+          tab.webContents.send("ondisconnected", reason);
+        });
+    
+        whatsAppService.bot.initialize().catch((err) => {
+          console.error(err);
+          tab.webContents.send("error", err);
+          dialog.showErrorBox("Ops!", err);
+        });
+      })
+      .catch((err) => {
         console.error(err);
         tab.webContents.send("error", err);
         dialog.showErrorBox("Ops!", err);
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      tab.webContents.send("error", err);
-      dialog.showErrorBox("Ops!", err);
-    })
-
+      })
+  })
 
   return tab
 }
