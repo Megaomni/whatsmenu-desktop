baf1b2ab136b1dba6ac1cd6e11acaced9d5f5e35
Bruno Santos <brsantos197@gmail.com>
2024-06-13 15:12:54 -0300

    feat: Versão do app no titulo da janela

--- src/windows/tabs-window.ts
+++ src/windows/tabs-window.ts
@@ -1,6 +1,6 @@
-import { WebContentsView, ipcMain, screen } from "electron";
+import { WebContentsView, app, ipcMain, screen } from "electron";
 
-import path from 'node:path';
+import path from "node:path";
 import { ProfileType } from "../@types/profile";
 import { registerShortCuts } from "../main/shortcuts";
 import { store } from "../main/store";
@@ -11,41 +11,52 @@ import { create_pdv_tab } from "./tabs/pdv-tab";
 import { TabBrowser } from "../extends/tab-browser";
 import { whatsmenu_menu } from "../main/menu";
 
-let forceClose = false
+let forceClose = false;
 
-export const tabsWindow =  {
-  forceCloseWindow: () => forceClose = true,
+export const tabsWindow = {
+  forceCloseWindow: () => (forceClose = true),
   createWindow: () => {
-    const mainScreen = screen.getPrimaryDisplay()
-    const { width, height } = mainScreen.size
+    const mainScreen = screen.getPrimaryDisplay();
+    const { width, height } = mainScreen.size;
 
     const window = new TabBrowser({
-      tabs: [create_dashboard_tab(), create_pdv_tab(), create_menu_tab(), create_bot_tab()],
+      tabs: [
+        create_dashboard_tab(),
+        create_pdv_tab(),
+        create_menu_tab(),
+        create_bot_tab(),
+      ],
       width,
       height,
-    })
+    });
 
-    window.setMenu(whatsmenu_menu)
-    window.maximize()
+    window.setMenu(whatsmenu_menu);
+    window.maximize();
     const tabGroup = new WebContentsView({
       webPreferences: {
-        preload: path.join(__dirname, 'preload.js'),
-        nodeIntegration: true
+        preload: path.join(__dirname, "preload.js"),
+        nodeIntegration: true,
       },
-    })
+    });
 
-    tabGroup.setBounds({ x: 0, y: 0, width, height: 42 })
-    console.log( { window: window.getBounds(), tabGroup: tabGroup.getBounds(), tabs: window.tabs.map(tab => tab.getBounds()) })
+    tabGroup.setBounds({ x: 0, y: 0, width, height: 42 });
+    console.log({
+      window: window.getBounds(),
+      tabGroup: tabGroup.getBounds(),
+      tabs: window.tabs.map((tab) => tab.getBounds()),
+    });
 
-    ipcMain.on('setActiveTab', (_, tabIndex) => {
+    ipcMain.on("setActiveTab", (_, tabIndex) => {
       window.tabs.forEach((tab) => {
-        tab.setVisible(tab.id === tabIndex)
-      })
-    })
+        tab.setVisible(tab.id === tabIndex);
+      });
+    });
 
     if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-      tabGroup.webContents.loadURL(`${MAIN_WINDOW_VITE_DEV_SERVER_URL}/src/views/tabs.html`);
-      window.setTitle("WhatsMenu Desktop");
+      tabGroup.webContents.loadURL(
+        `${MAIN_WINDOW_VITE_DEV_SERVER_URL}/src/views/tabs.html`
+      );
+      window.setTitle(`WhatsMenu Desktop - ${app.getVersion()}`);
     } else {
       tabGroup.webContents.loadFile(
         path.join(
@@ -54,25 +65,25 @@ export const tabsWindow =  {
         )
       );
     }
-    
-    window.contentView.addChildView(tabGroup)
-    window.tabs.forEach(tab => window.contentView.addChildView(tab))
-    tabGroup.webContents.on('did-finish-load', () => {
-      const profile = store.get('configs.profile') as ProfileType
-      tabGroup.webContents.send('onProfileChange', profile)
-      store.onDidChange('configs', (newValue) => {
-        tabGroup.webContents.send('onProfileChange', newValue.profile)
-      })
-    })
 
-    registerShortCuts(window)
-    window.on('close', (e) => {
+    window.contentView.addChildView(tabGroup);
+    window.tabs.forEach((tab) => window.contentView.addChildView(tab));
+    tabGroup.webContents.on("did-finish-load", () => {
+      const profile = store.get("configs.profile") as ProfileType;
+      tabGroup.webContents.send("onProfileChange", profile);
+      store.onDidChange("configs", (newValue) => {
+        tabGroup.webContents.send("onProfileChange", newValue.profile);
+      });
+    });
+
+    registerShortCuts(window);
+    window.on("close", (e) => {
       if (!forceClose) {
-        e.preventDefault()
-        window.minimize()
+        e.preventDefault();
+        window.minimize();
       }
-    })
+    });
 
-    return window
-  }
-}
+    return window;
+  },
+};
