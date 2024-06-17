5943f9860a8d556e93accab5dff3431307a61b6c
Bruno Santos <brsantos197@gmail.com>
2024-06-17 10:16:44 -0300

    style: prettier store.ts

--- src/main/store.ts
+++ src/main/store.ts
@@ -8,153 +8,199 @@ import { AxiosResponse } from "axios";
 export interface Store {
   configs: {
     printing: {
-      printers: Printer[]
-    }
+      printers: Printer[];
+    };
     whatsapp: {
-      showHiddenWhatsApp: boolean
-    },
-    executablePath?: string,
-    profile: ProfileType | null
-    contacts_cache: CacheContact[]
-    voucherToNotify: VoucherNotification[]
-  }
+      showHiddenWhatsApp: boolean;
+    };
+    executablePath?: string;
+    profile: ProfileType | null;
+    contacts_cache: CacheContact[];
+    voucherToNotify: VoucherNotification[];
+  };
 }
 
 export const store = new ElectronStore<Store>({
   watch: true,
   migrations: {
-    '0.2.2': (store) => {
-      store.set('configs.contacts_cache', [])
+    "0.2.2": (store) => {
+      store.set("configs.contacts_cache", []);
     },
-    '0.2.4': (store) => {
-      store.set('configs.voucherToNotify', [])
-    }
+    "0.2.4": (store) => {
+      store.set("configs.voucherToNotify", []);
+    },
   },
   defaults: {
     configs: {
       printing: {
-        printers: []
+        printers: [],
       },
       whatsapp: {
-        showHiddenWhatsApp: false
+        showHiddenWhatsApp: false,
       },
       profile: null,
       contacts_cache: [],
-      voucherToNotify: []
-    }
-  }
+      voucherToNotify: [],
+    },
+  },
 });
 
-export const getPrinters = () => store.get<'configs.printing.printers', Printer[]>('configs.printing.printers')
+export const getPrinters = () =>
+  store.get<"configs.printing.printers", Printer[]>(
+    "configs.printing.printers"
+  );
 
-export const getPrinter = (id: string) => store.get<'configs.printing.printers', Printer[]>('configs.printing.printers').find(p => p.id === id)
+export const getPrinter = (id: string) =>
+  store
+    .get<"configs.printing.printers", Printer[]>("configs.printing.printers")
+    .find((p) => p.id === id);
 
-export const addPrinter = (payload: Omit<Printer, 'options'>) => {
-  store.set('configs.printing.printers', [
-    ...getPrinters(),
-    payload
-  ])
-  return getPrinter(payload.id)
-}
+export const addPrinter = (payload: Omit<Printer, "options">) => {
+  store.set("configs.printing.printers", [...getPrinters(), payload]);
+  return getPrinter(payload.id);
+};
 export const updatePrinter = (payload: Partial<Printer>) => {
-  const printer = store.get<'configs.printing.printers', Printer[]>('configs.printing.printers').find(p => p.id === payload.id)
+  const printer = store
+    .get<"configs.printing.printers", Printer[]>("configs.printing.printers")
+    .find((p) => p.id === payload.id);
   if (printer) {
-    const printers = getPrinters()
-    const printersUpdated = printers.map(p => {
+    const printers = getPrinters();
+    const printersUpdated = printers.map((p) => {
       if (p.id === payload.id) {
         return {
           ...p,
-          ...payload
-        }
+          ...payload,
+        };
       }
-      return p
-    })
-    store.set('configs.printing.printers', printersUpdated)
+      return p;
+    });
+    store.set("configs.printing.printers", printersUpdated);
   }
-}
+};
 
-export const deletePrinter = (id: string) => store.set('configs.printing.printers', (store.get('configs.printing.printers') as Printer[]).filter(p => p.id !== id))
+export const deletePrinter = (id: string) =>
+  store.set(
+    "configs.printing.printers",
+    (store.get("configs.printing.printers") as Printer[]).filter(
+      (p) => p.id !== id
+    )
+  );
 
-export const getProfile = () => store.get<'configs.profile', ProfileType>('configs.profile')
+export const getProfile = () =>
+  store.get<"configs.profile", ProfileType>("configs.profile");
 
-export const setCacheContactList = (cacheContact: CacheContact) => store.set('configs.contacts_cache', cacheContact)
+export const setCacheContactList = (cacheContact: CacheContact) =>
+  store.set("configs.contacts_cache", cacheContact);
 
-export const getCacheContactList = () => store.get<'configs.contacts_cache', Store['configs']['contacts_cache']>('configs.contacts_cache')
+export const getCacheContactList = () =>
+  store.get<"configs.contacts_cache", Store["configs"]["contacts_cache"]>(
+    "configs.contacts_cache"
+  );
 
-export const setCacheContactByWhatsapp = (whatsapp: string, payload: Partial<CacheContact>) => {
-  const cacheList = getCacheContactList()
-  const cacheListUpdated = cacheList.map(cached => {
+export const setCacheContactByWhatsapp = (
+  whatsapp: string,
+  payload: Partial<CacheContact>
+) => {
+  const cacheList = getCacheContactList();
+  const cacheListUpdated = cacheList.map((cached) => {
     if (cached.contact === whatsapp) {
       return {
         ...cached,
-        ...payload
-      }
+        ...payload,
+      };
     }
-    return cached
-  })
-  store.set('configs.contacts_cache', cacheListUpdated)
-}
+    return cached;
+  });
+  store.set("configs.contacts_cache", cacheListUpdated);
+};
 
 export const findCacheContact = async (whatsapp: string) => {
-  const cacheList = getCacheContactList()
-  const profile = getProfile()
-  whatsapp = whatsapp?.substring(2).replaceAll(/\D/g, '')
-  let contact = whatsapp
-  let cache = cacheList.find(cached => cached?.contact === whatsapp) 
+  const cacheList = getCacheContactList();
+  const profile = getProfile();
+  whatsapp = whatsapp?.substring(2).replaceAll(/\D/g, "");
+  let contact = whatsapp;
+  let cache = cacheList.find((cached) => cached?.contact === whatsapp);
   if (whatsapp) {
-    if (cache && cache.messageType === 'welcome') {
-      return cache
+    if (cache && cache.messageType === "welcome") {
+      return cache;
     }
 
     if (contact) {
-      if (!cache || cache.messageType === 'cupomFirst') {
-        let response: AxiosResponse
+      if (!cache || cache.messageType === "cupomFirst") {
+        let response: AxiosResponse;
 
-        if (!cache || (cache && DateTime.fromISO(cache.created_at).diffNow('hours').hours >= cache.revalidateTime)) {
+        if (
+          !cache ||
+          (cache &&
+            DateTime.fromISO(cache.created_at).diffNow("hours").hours >=
+              cache.revalidateTime)
+        ) {
           try {
-            response = await whatsmenu_api_v3.get(`/findClient?whatsapp=${whatsapp}&profileId=${profile?.id}`)
+            response = await whatsmenu_api_v3.get(
+              `/findClient?whatsapp=${whatsapp}&profileId=${profile?.id}`
+            );
           } catch (error) {
-            return null
+            return null;
           }
           if (response.data.client?.whatsapp) {
-            contact = response.data.client?.whatsapp
+            contact = response.data.client?.whatsapp;
           }
-          cache = { 
-            contact, 
-            messageType: !response.data.client?.last_requests.length && profile?.firstOnlyCupom ? 'cupomFirst' : 'welcome', 
+          cache = {
+            contact,
+            messageType:
+              !response.data.client?.last_requests.length &&
+              profile?.firstOnlyCupom
+                ? "cupomFirst"
+                : "welcome",
             created_at: DateTime.local().toISO(),
-            revalidateTime: 3
-          }
-          store.set('configs.contacts_cache', [...cacheList, cache])
+            revalidateTime: 3,
+          };
+          store.set("configs.contacts_cache", [...cacheList, cache]);
         }
       }
     }
   }
-  return cache
-}
+  return cache;
+};
 
-export const storeVoucherToNotify = (payload: VoucherNotification) => store.set('configs.voucherToNotify', [...getVoucherToNotifyList(), payload])
+export const storeVoucherToNotify = (payload: VoucherNotification) =>
+  store.set("configs.voucherToNotify", [...getVoucherToNotifyList(), payload]);
 
 export const getVoucherToNotifyList = () => {
-  const vouchersToNotify = store.get<'configs.voucherToNotify', VoucherNotification[]>('configs.voucherToNotify')
+  const vouchersToNotify = store.get<
+    "configs.voucherToNotify",
+    VoucherNotification[]
+  >("configs.voucherToNotify");
   if (!vouchersToNotify) {
-    store.set('configs.voucherToNotify', [])
+    store.set("configs.voucherToNotify", []);
   }
-  return store.get<'configs.voucherToNotify', VoucherNotification[]>('configs.voucherToNotify')
-}
+  return store.get<"configs.voucherToNotify", VoucherNotification[]>(
+    "configs.voucherToNotify"
+  );
+};
 
-export const deleteVoucherToNotify = (id: number) => store.set('configs.voucherToNotify', getVoucherToNotifyList().filter(voucher => voucher.id !== id))
+export const deleteVoucherToNotify = (id: number) =>
+  store.set(
+    "configs.voucherToNotify",
+    getVoucherToNotifyList().filter((voucher) => voucher.id !== id)
+  );
 
-export const updateVoucherToNotify = (id: number, payload: Partial<VoucherNotification>) => {
-  store.set('configs.voucherToNotify', getVoucherToNotifyList().map(voucher => {
-    if (voucher.id === id) {
-      return {
-        ...voucher,
-        ...payload
+export const updateVoucherToNotify = (
+  id: number,
+  payload: Partial<VoucherNotification>
+) => {
+  store.set(
+    "configs.voucherToNotify",
+    getVoucherToNotifyList().map((voucher) => {
+      if (voucher.id === id) {
+        return {
+          ...voucher,
+          ...payload,
+        };
       }
-    }
-    return voucher
-  }))
-}
+      return voucher;
+    })
+  );
+};
 
 console.log(store.path);
