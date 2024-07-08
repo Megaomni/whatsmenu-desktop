337d12e731713ab21ddf2cd794efe76ae5602a0b
Bruno Santos <brsantos197@gmail.com>
2024-07-08 13:56:27 -0300

    refact: Ajuste service ifood
    - removido `profile` e `merchant` do arquivo
    - getMerchant agora recebe um objeto como argumento
    - polling  agora recebe um objeto como argumento

--- src/services/ifood.ts
+++ src/services/ifood.ts
@@ -1,115 +1,151 @@
 import axios from "axios";
-import { whatsmenu_api_v3, integration_api } from "../lib/axios";
-import { getMerchant, getProfile, store } from "../main/store";
-import {io} from '../services/ws_integration'
-import { DateTime } from "luxon";
-import  { ProfileType } from "../@types/profile";
+import { MerchantType } from "../@types/merchant";
+import { ProfileType } from "../@types/profile";
+import { integration_api, whatsmenu_api_v3 } from "../lib/axios";
+import { store } from "../main/store";
+import { io } from "../services/ws_integration";
 
+let pollingData;
 
-const profile = getProfile()
-const merchant = getMerchant()
-let pollingData
+export const getMerchantApi = async ({ profile }: { profile: ProfileType }) => {
+  try {
+    if (!profile) {
+      throw new Error("Perfil não encontrado!");
+    }
+    const { data } = await whatsmenu_api_v3.get(
+      `/merchant?slug=${profile.slug}`
+    );
+    store.set("configs.merchant", data);
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const getMerchantApi = async (profile: ProfileType) => {
-      try {
-        if(!profile){
-          throw new Error('Perfil não encontrado')
-        }
-        console.log('PEGANDO O MERCHANT')
-        const { data } = await whatsmenu_api_v3.get(
-          `/merchant?slug=${profile.slug}`
-        );
-        store.set("configs.merchant", data);
-      } catch (error) {
-        console.error(error);
-      }
-}  
-
-export const polling = async () => {
-
-    try {
-      console.log('vai chamar o polling')
-      const { data } = await integration_api.get('https://merchant-api.ifood.com.br/events/v1.0/events:polling?groups=ORDER_STATUS', {
+export const polling = async ({
+  merchant,
+  profile,
+}: {
+  profile: ProfileType;
+  merchant: MerchantType;
+}) => {
+  try {
+    const { data } = await integration_api.get(
+      "https://merchant-api.ifood.com.br/events/v1.0/events:polling?groups=ORDER_STATUS",
+      {
         headers: {
-          'Authorization': `Bearer ${merchant?.token}`,
-          'x-polling-merchants': `${merchant?.merchantId}`,
-        }
-      })
+          Authorization: `Bearer ${merchant?.token}`,
+          "x-polling-merchants": `${merchant?.merchantId}`,
+        },
+      }
+    );
 
-      pollingData = data
-      if(pollingData.length > 0) {
-        sendPollingDataApi(pollingData, profile.id, profile.slug)
-        pollingAcknowledgment(pollingData)
-      }
-
-    } catch (error) {
-      if(error.response.status === 401) {
-        console.log('DEU 401 TOKEN EXPIRADO')
-        attToken(profile)
-      }
-      if (error.response) {
-        console.error('Server responded with status code:', error.response.status)
-        console.error('Response data:', error.response.data)
-      } else if (error.request) {
-        console.error('No response received:', error.request)
-      } else {
-        console.error('Error creating request:', error.message)
-      }
-      throw error
+    pollingData = data;
+    if (pollingData.length > 0) {
+      sendPollingDataApi(pollingData, profile.id, profile.slug);
+      pollingAcknowledgment(pollingData);
     }
-  }
-
-  const sendPollingDataApi = async (pollingData: [], id: number, slug: string) => {
-    try {
-      let returnOrders
-      console.log('vai enviar o polling pra API')
-      console.log('PEDIDOS PARA POLLING', pollingData)
-      if(pollingData.length > 0) {
-        returnOrders = await whatsmenu_api_v3.post('ifood/polling', { pollingData, id, slug})
-      } 
-      if(returnOrders) {
-        io.to(`ifood:${profile?.slug}`).emit('newOrderIfood', returnOrders.data)
-      }
-    } catch (error) {
-      console.error('erro ao enviar dados do Polling para API integração', error)
+  } catch (error) {
+    if (error.response.status === 401) {
+      console.log("DEU 401 TOKEN EXPIRADO");
+      attToken(profile);
     }
+    if (error.response) {
+      console.error(
+        "Server responded with status code:",
+        error.response.status
+      );
+      console.error("Response data:", error.response.data);
+    } else if (error.request) {
+      console.error("No response received:", error.request);
+    } else {
+      console.error("Error creating request:", error.message);
+    }
+    throw error;
   }
+};
 
-  const pollingAcknowledgment = async (pollingData: []) => {
-    try {
-      console.log('VAI FAZER O RECONHECIMENTO DO POLLING')
-      const { data } = await axios.post('https://merchant-api.ifood.com.br/events/v1.0/events/acknowledgment',pollingData ,{
+const sendPollingDataApi = async (
+  pollingData: [],
+  id: number,
+  slug: string
+) => {
+  try {
+    let returnOrders;
+    console.log("vai enviar o polling pra API");
+    console.log("PEDIDOS PARA POLLING", pollingData);
+    if (pollingData.length > 0) {
+      returnOrders = await whatsmenu_api_v3.post("ifood/polling", {
+        pollingData,
+        id,
+        slug,
+      });
+    }
+    if (returnOrders) {
+      io.to(`ifood:${profile?.slug}`).emit("newOrderIfood", returnOrders.data);
+    }
+  } catch (error) {
+    console.error("erro ao enviar dados do Polling para API integração", error);
+  }
+};
+
+const pollingAcknowledgment = async (pollingData: []) => {
+  try {
+    console.log("VAI FAZER O RECONHECIMENTO DO POLLING");
+    const { data } = await axios.post(
+      "https://merchant-api.ifood.com.br/events/v1.0/events/acknowledgment",
+      pollingData,
+      {
         headers: {
-          'Authorization': `Bearer ${merchant?.token}`,
-        }
-      })
-    } catch (error) {
-      console.error('erro ao fazer o reconhecimento pelo ifood', error)
+          Authorization: `Bearer ${merchant?.token}`,
+        },
+      }
+    );
+  } catch (error) {
+    console.error("erro ao fazer o reconhecimento pelo ifood", error);
+  }
+};
+
+export const attToken = async (profile: ProfileType) => {
+  try {
+    console.log("VAI GERAR UM NOVO TOKEN");
+
+    const { data } = await integration_api.post("/ifood/refreshToken", {
+      id: profile.id,
+    });
+
+    if (data) {
+      console.log("GERADO TOKEN");
+    } else {
+      console.log("NÃO GEROU O TOKEN");
     }
+  } catch (error) {
+    if (error.response) {
+      console.error(
+        "Server responded with status code:",
+        error.response.status
+      );
+      console.error("Response data:", error.response.data);
+    } else if (error.request) {
+      console.error("No response received:", error.request);
+    } else {
+      console.error("Error creating request:", error.message);
+    }
+    throw error;
+  }
+  const { data } = await integration_api.post(
+    "/ifood/refreshToken",
+    profile.id
+  );
+  console.log("DATA DO ATT TOKEN", data);
+
+  if (data) {
+    console.log("GERADO TOKEN");
+  } else {
+    console.log("NÃO GEROU O TOKEN");
   }
 
-  export const attToken = async (profile: ProfileType) => {
-    try {
-      console.log('VAI GERAR UM NOVO TOKEN')
-      
-      const { data } = await integration_api.post('/ifood/refreshToken', {
-        id: profile.id} )
-      
-        if(data) {
-          console.log("GERADO TOKEN");
-        } else {
-          console.log("NÃO GEROU O TOKEN");
-        }
-        
-    } catch (error) {
-      if (error.response) {
-        console.error('Server responded with status code:', error.response.status)
-        console.error('Response data:', error.response.data)
-      } else if (error.request) {
-        console.error('No response received:', error.request)
-      } else {
-        console.error('Error creating request:', error.message)
-      }
-      throw error
-    }
-  }
+  merchant.token = data.accessToken;
+  merchant.refresh_token = data.refreshToken;
+
+  store.set("configs.merchant", merchant);
+};
--- src/windows/tabs/dashboard-tab.ts
+++ src/windows/tabs/dashboard-tab.ts
@@ -1,68 +1,74 @@
-
-import path from "node:path"
-import { WebTabContentsView } from "../../extends/tab"
-import { getMerchant, getProfile, store } from "../../main/store"
-import { DateTime } from "luxon"
-import { WeekDayType } from "../../@types/week"
-import { attToken, getMerchantApi, polling } from "../../services/ifood"
+import { DateTime } from "luxon";
+import path from "node:path";
+import { WeekDayType } from "../../@types/week";
+import { WebTabContentsView } from "../../extends/tab";
+import { getMerchant, getProfile, store } from "../../main/store";
+import { getMerchantApi, polling } from "../../services/ifood";
 
 export const create_dashboard_tab = () => {
-  
   const tab = new WebTabContentsView({
-    id: 'dashboard',
+    id: "dashboard",
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
-  })
-  
+  });
+
   // tab.webContents.loadURL(`https://next.whatsmenu.com.br/`)
   // tab.webContents.loadURL(`https://teste.whatsmenu.com.br/`)
-  tab.webContents.loadURL(`http://localhost:3000`)
+  tab.webContents.loadURL(`http://localhost:3000`);
 
   tab.webContents.on("did-finish-load", () => {
     const profile = getProfile();
-    let merchant: any
-    let open = false
+    let merchant: any;
+    let open = false;
     if (profile) {
-
-      if(!merchant){
-        console.log('não tem o merchant')
+      if (!merchant) {
+        console.log("não tem o merchant");
       } else {
-        console.log('PEGOU O merchant')
+        console.log("PEGOU O merchant");
       }
 
-      const day = DateTime.local().setZone(profile.timeZone).toISO()
-      const today = DateTime.fromISO(day, { zone: profile.timeZone }).toFormat('EEEE').toLowerCase()
-      const convert = (text: string) => parseFloat(text.replace(':', '.'))
-  
+      const day = DateTime.local().setZone(profile.timeZone).toISO();
+      const today = DateTime.fromISO(day, { zone: profile.timeZone })
+        .toFormat("EEEE")
+        .toLowerCase();
+      const convert = (text: string) => parseFloat(text.replace(":", "."));
+
       if (!profile.week[today]) {
-        open = false
+        open = false;
       }
-      const now = parseFloat(DateTime.local().setZone(profile.timeZone).toFormat('HH.mm'))
-      const filter = profile.week[today].filter((d: WeekDayType) => now >= convert(d.open) && now <= convert(d.close))
-  
+      const now = parseFloat(
+        DateTime.local().setZone(profile.timeZone).toFormat("HH.mm")
+      );
+      const filter = profile.week[today].filter(
+        (d: WeekDayType) => now >= convert(d.open) && now <= convert(d.close)
+      );
+
       if (filter.length) {
-        open = true
+        open = true;
       }
 
-      if(open){
+      if (open) {
         console.log("ABERTO");
       } else {
         console.log("FECHADO");
       }
-      
+
       store.onDidAnyChange((newValue, oldValue) => {
-        console.log(oldValue.configs.profile.options.integrations, newValue.configs.profile.options.integrations)
-        if(newValue.configs.profile.options.integrations) {
-          getMerchantApi(profile)
+        console.log(
+          oldValue.configs.profile.options.integrations,
+          newValue.configs.profile.options.integrations
+        );
+        if (newValue.configs.profile.options.integrations) {
+          getMerchantApi({ profile });
           merchant = getMerchant();
           if (open && merchant) {
             setInterval(polling, 30000);
           }
         }
-      })
+      });
     }
-  })
+  });
 
-  return tab
-}
+  return tab;
+};
