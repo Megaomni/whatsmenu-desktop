fb0276b0409a573bd53e73c9de1c7431e27579e1
Germano Martins <germanopmartins91@gmail.com>
2024-07-12 17:31:59 -0300

    feat: Criando variaveis de ambiente
    - Criada as variaveis Dashboard, Store e Websocket
    
    - Alterada as passagens dessas varias nos arquivos tab's
    
    - Adicionada variaveis ao schema do zod

--- src/windows/tabs/menu-tab.ts
+++ src/windows/tabs/menu-tab.ts
@@ -1,28 +1,27 @@
-
-import path from "node:path"
-import { ProfileType } from "../../@types/profile"
-import { store } from "../../main/store"
-import { WebTabContentsView } from "../../extends/tab"
+import path from "node:path";
+import { ProfileType } from "../../@types/profile";
+import { store } from "../../main/store";
+import { WebTabContentsView } from "../../extends/tab";
+import { env } from "../../environments";
 
 export const create_menu_tab = () => {
-  
-  const profile = store.get('configs.profile') as ProfileType
+  const profile = store.get("configs.profile") as ProfileType;
   const tab = new WebTabContentsView({
-    id: 'menu',
+    id: "menu",
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
-  })
-  
-  tab.webContents.loadURL(`https://whatsmenu.com.br/${profile?.slug}`)
-  
-  tab.setVisible(false)
+  });
 
-  store.onDidChange('configs', (newValue) => {
+  tab.webContents.loadURL(env.WM_STORE + `${profile?.slug}`);
+
+  tab.setVisible(false);
+
+  store.onDidChange("configs", (newValue) => {
     if (profile?.slug !== newValue.profile?.slug) {
-      tab.webContents.loadURL(`https://whatsmenu.com.br/${newValue.profile.slug}`)
+      tab.webContents.loadURL(env.WM_STORE + `${newValue.profile.slug}`);
     }
-  })
+  });
 
-  return tab
-}
+  return tab;
+};
