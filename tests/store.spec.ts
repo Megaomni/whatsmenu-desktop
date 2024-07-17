e2c93f387af9b578ac7cc6f4f41600ee979c7187
Germano Martins <germanopmartins91@gmail.com>
2024-07-17 17:57:38 -0300

    feat: metodo removeDuplicate
    test para o método e função comentada.

--- tests/store.spec.ts
+++ tests/store.spec.ts
@@ -1,6 +1,10 @@
 import { describe, it, vi, expect, beforeEach, afterEach } from "vitest";
 import voucherToNotifyMock from "./mocks/voucherToNotify.json";
-import { store, storeVoucherToNotify } from "../src/main/store";
+import {
+  store,
+  storeVoucherToNotify,
+  removeDuplicateVouchers,
+} from "../src/main/store";
 
 describe.only("storeVoucherToNotify", () => {
   let getSpy: ReturnType<typeof vi.spyOn>;
@@ -18,6 +22,7 @@ describe.only("storeVoucherToNotify", () => {
   it("Deve adicionar um novo voucher se ele não existir", async () => {
     getSpy.mockReturnValue([]);
     storeVoucherToNotify(voucherToNotifyMock[0]);
+
     expect(setSpy).toHaveBeenCalledWith("configs.voucherToNotify", [
       voucherToNotifyMock[0],
     ]);
@@ -26,6 +31,18 @@ describe.only("storeVoucherToNotify", () => {
   it("Não deve adicionar o voucher se ele já existir", async () => {
     getSpy.mockReturnValue([voucherToNotifyMock[0]]);
     storeVoucherToNotify(voucherToNotifyMock[0]);
+
     expect(setSpy).not.toHaveBeenCalled();
+  });
+
+  it("Deve remover vouchers duplicados", async () => {
+    const mockVouchers = [...voucherToNotifyMock, voucherToNotifyMock[0]];
+    getSpy.mockReturnValue(mockVouchers);
+    removeDuplicateVouchers();
+
+    expect(setSpy).toHaveBeenCalledWith(
+      "configs.voucherToNotify",
+      voucherToNotifyMock
+    );
   });
 });
