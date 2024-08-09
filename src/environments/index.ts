64136eb34f905528ceb0ad67ab67df1de9b801fc
Bruno Santos <brsantos197@gmail.com>
2024-08-09 16:02:56 -0300

    Squashed 'apps/desktop_app/' content from commit 287fd1730
    git-subtree-dir: apps/desktop_app
    git-subtree-split: 287fd1730f16c8bc9a43ac9e738f1452af57ddb9

---
+++ src/environments/index.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  WM_API_V3: z.string(),
+  WM_API_V2: z.string(),
+  WM_DASHBOARD: z.string(),
+  WM_STORE: z.string(),
+  WM_WEBSOCKET: z.string(),
+});
+
+export type Env = z.infer<typeof envSchema>;
+
+export let env: Env = {};
+
+if (typeof process === "undefined" && window) {
+  env = envSchema.parse(window.env());
+} else {
+  env = envSchema.parse(process.env);
+}
