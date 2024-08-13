56eb6fa5de378c5ec8fabe194ac25e98170630d1
Bruno Santos <brsantos197@gmail.com>
2024-08-13 19:40:08 -0300

    new: Api CI config

---
+++ apps/api/v2/ecosystem_beta.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'beta',
+      script: 'npm',
+      args: 'start',
+      combine_logs: true,
+      time: true,
+      max_memory_restart: '1G',
+    },
+  ],
+};
+//
