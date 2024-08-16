35ed421ea7918c1acf4de055fa49f474cd87bab7
Wagner Vital <wagnervitaldo@gmail.com>
2024-08-16 15:54:49 -0300

    #4 feat: layout em percurso de criação de empresa

--- apps/dashboard/src/components/GroveNFe/CreateCompany/index.tsx
+++ apps/dashboard/src/components/GroveNFe/CreateCompany/index.tsx
@@ -1,6 +1,10 @@
-import { Button, Card, Col, Form, FormGroup, Row, Tab, Tabs } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Card, Col, Form, FormGroup, Nav, Row, Tab, Tabs } from "react-bootstrap";
 
 export function CreateCompany() {
+    const [advancedSettings, setAdvancedSettings] = useState(false)
+
+    const toggleAdvancedSettings = () => setAdvancedSettings(!advancedSettings)
     return (
         <>
             <form>
@@ -34,46 +38,181 @@ export function CreateCompany() {
 
                 <Card>
                     <Card.Body>
-                        <Tabs className="d-flex flex-wrap overflow-auto" style={{ display: 'flex !important' }}>
-
-                            <Tab eventKey='identification' title='Identificação' className="mt-4">
-                                <Button style={{ backgroundColor: '#13C296', border: 'none' }} >Anexar Logo da Empresa</Button>
-                                <Row>
-                                    <Col md={3}>
-                                        <Form.Label className="m-0 p-0 mt-4">Nome Fantasia</Form.Label>
-                                        <Form.Control></Form.Control>
-                                    </Col>
-                                    <Col md={3}>
-                                        <Form.Label className="m-0 p-0 mt-4">Inscrição Estadual</Form.Label>
-                                        <Form.Control></Form.Control>
-                                    </Col>
-                                    <Col md={3}>
-                                        <Form.Label className="m-0 p-0 mt-4">Inscrição Municipal</Form.Label>
-                                        <Form.Control></Form.Control>
-                                    </Col>
-                                    <Col md={3}>
-                                        <Form.Label className="m-0 p-0 mt-4">Regime Tributário</Form.Label>
-                                        <Form.Select>
-                                            <option value="1"> Simples Nacional</option>
-                                            <option value="2">Simples Nacional - Excesso de sublimite de receita bruta</option>
-                                            <option value="3">Regime Normal</option>
-                                        </Form.Select>
-                                    </Col>
-                                </Row>
-                            </Tab>
+                        <Tab.Container>
 
 
-                            <Tab eventKey='contact' title='Contato'></Tab>
-                            <Tab eventKey='address' title='Endereço'></Tab>
-                            <Tab eventKey='responsable' title='Responsável'></Tab>
-                            <Tab eventKey='accounting' title='Contabilidade'></Tab>
-                            <Tab eventKey='tokens' title='Tokens'></Tab>
-                            <Tab eventKey='docFiscal' title='Documentos Fiscais'></Tab>
-                            <Tab eventKey='config' title='Configurações'></Tab>
-                        </Tabs>
+                            <Nav className="d-flex flex-row overflow-auto m-0 p-0" style={{ whiteSpace: 'nowrap' }}>
+                                <Nav.Item >
+                                    <Nav.Link eventKey='identification'>Identificação</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item >
+                                    <Nav.Link eventKey='contact'>Contato</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item >
+                                    <Nav.Link eventKey='address'>Endereço</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item >
+                                    <Nav.Link eventKey='responsable'>Responsável</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item >
+                                    <Nav.Link eventKey='accounting'>Contabilidade</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item >
+                                    <Nav.Link eventKey='tokens'>Tokens</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item >
+                                    <Nav.Link eventKey='docFiscal'>Documentos Fiscais</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item >
+                                    <Nav.Link eventKey='config'>Configurações</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content className="mt-4">
+                                <Tab.Pane eventKey='identification'>
+                                    <Button style={{ backgroundColor: '#13C296', border: 'none' }} >Anexar Logo da Empresa</Button>
+                                    <Row>
+                                        <Col md={3}>
+                                            <Form.Label className="m-0 p-0 mt-4">Nome Fantasia</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col md={3}>
+                                            <Form.Label className="m-0 p-0 mt-4">Inscrição Estadual</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col md={3}>
+                                            <Form.Label className="m-0 p-0 mt-4">Inscrição Municipal</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col md={3}>
+                                            <Form.Label className="m-0 p-0 mt-4">Regime Tributário</Form.Label>
+                                            <Form.Select>
+                                                <option value="1"> Simples Nacional</option>
+                                                <option value="2">Simples Nacional - Excesso de sublimite de receita bruta</option>
+                                                <option value="3">Regime Normal</option>
+                                            </Form.Select>
+                                        </Col>
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey='contact'>
+                                    <Row>
+                                        <Col md={3}>
+                                            <Form.Label className="m-0 p-0 mt-4">Email</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col md={3}>
+                                            <Form.Label className="m-0 p-0 mt-4">Telefone</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey='address'>
+                                    <Row>
+                                        <Col md={2}>
+                                            <Form.Label className="m-0 p-0 mt-4">CEP</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col md={4}>
+                                            <Form.Label className="m-0 p-0 mt-4">Logradouro</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col xs={6} md={3}>
+                                            <Form.Label className="m-0 p-0 mt-4">Número</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col xs={6} md={3}>
+                                            <Form.Label className="m-0 p-0 mt-4">Complemento</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col xs={6} md={4}>
+                                            <Form.Label className="m-0 p-0 mt-4">Bairro</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col xs={6} md={3}>
+                                            <Form.Label className="m-0 p-0 mt-4">Municipio</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col md={2}>
+                                            <Form.Label className="m-0 p-0 mt-4">Uf</Form.Label>
+                                            <Form.Select>
+                                                <option value="1"> Simples Nacional</option>
+                                            </Form.Select>
+                                        </Col>
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey='responsable'>
+                                    <Row>
+                                        <Col md={4}>
+                                            <Form.Label className="m-0 p-0 mt-4">Nome do Responsável</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col md={4}>
+                                            <Form.Label className="m-0 p-0 mt-4">CPF do Responsável</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey='accounting'>
+                                    <Row>
+                                        <Col md={4}>
+                                            <Form.Label className="m-0 p-0 mt-4">CPF/CNPJ</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey='tokens'>
+                                    <Row>
+                                        <Col md={4}>
+                                            <Form.Label className="m-0 p-0 mt-4">Token de Homologação</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                        <Col md={4}>
+                                            <Form.Label className="m-0 p-0 mt-4">Token de Produção</Form.Label>
+                                            <Form.Control></Form.Control>
+                                        </Col>
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey='docFiscal'>
+                                    <Form.Switch label='NFe' className="pb-3" style={{ borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}></Form.Switch>
+                                    <Form.Switch label='NFCe' className="pb-3 mt-3" style={{ borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}></Form.Switch>
+                                    <Form.Switch label='NFSe' className="pb-3 mt-3" style={{ borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}></Form.Switch>
+                                    <Form.Switch label='CTe' className="pb-3 mt-3" style={{ borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}></Form.Switch>
+                                    <Form.Switch label='MDFe' className="pb-3 mt-3" style={{ borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}></Form.Switch>
+                                    <Form.Switch label='Recebimento de NFes' className="pb-3 mt-3" style={{ borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}></Form.Switch>
+                                    <Form.Switch label='Recebimento de CTes' className="pb-3 mt-3"></Form.Switch>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey='config'>
+                                    <div className="d-flex align-itens-center" style={{ borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}>
+                                        <Form.Switch className="d-flex align-items-center" ></Form.Switch>
+                                        <Form.Label className="ms-3 mt-3">(Todos os documentos) Enviar email ao destinatário - Produção</Form.Label>
+                                    </div>
+                                    <div className="d-flex align-itens-center" style={{ borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}>
+                                        <Form.Switch className="d-flex align-items-center" ></Form.Switch>
+                                        <Form.Label className="ms-3 mt-3">(Todos os documentos) Enviar email ao destinatário - Homologação</Form.Label>
+                                    </div>
+                                    <div className="d-flex align-itens-center" style={{ borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}>
+                                        <Form.Switch className="d-flex align-items-center" ></Form.Switch>
+                                        <Form.Label className="ms-3 mt-3">(NFe, NFCe) Discrimina impostos</Form.Label>
+                                    </div>
+                                    <div className="mt-3">
+                                        <p className="ms-3" onClick={() => toggleAdvancedSettings()} style={{ color: 'red', textDecoration: 'underline' }}>Configurações avançadas</p>
+                                        {advancedSettings &&
+                                            <div className="d-flex align-itens-center">
+
+                                                <Form.Switch className="d-flex align-items-center" style={{ backgroundColor: 'red', borderColor: 'red !important', boxShadow: 'none' }}></Form.Switch>
+                                                <Form.Label className="ms-3">Mostrar badge Focus NFe na DANFSe</Form.Label>
+                                            </div>
+                                        }
+                                    </div>
+                                </Tab.Pane>
+
+                            </Tab.Content>
+
+
+
+                        </Tab.Container>
                     </Card.Body>
                 </Card>
-            </form>
+            </form >
         </>
     )
 }
