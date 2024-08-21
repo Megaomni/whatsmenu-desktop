e4963008870ab6f76e2293ea9ce0087fb9074eaa
Bruno Santos <brsantos197@gmail.com>
2024-08-21 17:36:41 -0300

    fix: Ajuste asdasdas

--- apps/dashboard/src/components/Profile/FormsPayment/methods/pix.tsx
+++ apps/dashboard/src/components/Profile/FormsPayment/methods/pix.tsx
@@ -158,7 +158,7 @@ const OnlinePaymentSettings = ({ formPayment }: PaymentSettingsProps) => {
                   >
                     {keyArray?.map((type) => (
                       <option value={type} key={type}>
-                        {type} asdasdas
+                        {type}
                       </option>
                     ))}
                   </Form.Select>
