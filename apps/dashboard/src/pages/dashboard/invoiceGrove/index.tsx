c0ecfbdfb9d963e165efa9c18adfd4d0c805956c
LuanS <luansellera98@gmail.com>
2024-08-23 12:46:26 -0300

    new: invoiceGroveNFe

--- apps/dashboard/src/pages/dashboard/invoiceGrove/index.tsx
+++ apps/dashboard/src/pages/dashboard/invoiceGrove/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import { Container, Row, Col, Button, Card, Table } from 'react-bootstrap'
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Card,
+  Table,
+  Badge,
+} from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import { Title } from '../../../components/Partials/title'
 
@@ -16,82 +24,77 @@ export default function InvoicesGroveNFe({}: InvoicesProps) {
       <Container className="m-auto p-1">
         {/* Seção de Plano Atual */}
         <Row className="mb-4">
-          <Col>
-            <Card>
-              <Card.Header
-                className="d-flex justify-content-center align-items-center text-nowrap p-2"
-                style={{
-                  backgroundColor: '#126DFC',
-                  color: 'white',
-                }}
-              >
-                <h2 className="font-weight-bold fs-4 ms-3">Plano Atual:</h2>
-                <p className="font-weight-bold m-auto ms-2">Plano 100</p>
-                <Button variant="link" className="fw-light me-3  text-white">
-                  Mudar
-                </Button>
-              </Card.Header>
-              <Card.Body
-                className="align-items-center text-nowrap"
-                style={{ backgroundColor: '#E6E6E6' }}
-              >
-                <Col md={2} lg={3} className="d-flex">
-                  <p style={{ color: '#126DFC' }} className="fw-bold">
-                    Vencimento:
-                  </p>
-                  <p className="fw-bold ms-2">03/10/2024</p>
-                  <Button
-                    variant="link"
-                    className="text-danger ms h-100 me-2 ms-auto p-0 text-end"
-                  >
-                    Pagar
-                  </Button>
-                </Col>
-                <Col md={2} className="d-flex">
-                  <p style={{ color: '#126DFC' }} className="fw-bold">
-                    Valor:
-                  </p>
-                  <p className="fw-bold ms-2">R$67,00</p>
-                </Col>
-                <Col md={2} className="d-flex">
-                  <p style={{ color: '#126DFC' }} className="fw-bold">
-                    Adicionais:
-                  </p>
-                  <p className="fw-bold ms-2">R$10,00</p>
-                </Col>
-                <Col md={2} className="d-flex">
-                  <p style={{ color: '#126DFC' }} className="fw-bold">
-                    Formas de Pagamento:
-                  </p>
-                  <p className="fw-bold ms-2">Boleto</p>
-                  <Button
-                    variant="link"
-                    className="text-danger h-100 me-2 ml-auto p-0 text-end"
-                    style={{ marginLeft: 'auto' }}
-                  >
-                    Pagar
-                  </Button>
-                </Col>
-                <Col md={2} className="d-flex">
-                  <p style={{ color: '#126DFC' }} className="fw-bold">
-                    Total:
-                  </p>
-                  <p className="fw-bold ms-2">R$77,00</p>
-                </Col>
-              </Card.Body>
-            </Card>
-            <Col md={2} className="d-flex ms-auto">
-              <Button
-                className="fw-bold w-100 p-3 text-end"
-                style={{
-                  backgroundColor: '#13c296',
-                  color: 'white',
-                  border: 'none',
-                }}
-              >
-                Pagar Agora
+          <Card>
+            <Card.Header
+              className="d-flex align-items-center justify-content-center gap-3"
+              style={{
+                backgroundColor: '#126DFC',
+                color: 'white',
+              }}
+            >
+              <h2 className="font-weight-bold fs-4">Plano Atual:</h2>
+              <span className="font-weight-bold">Plano 100</span>
+              <Button variant="link" className="fw-bold btn-sm text-white">
+                Mudar
               </Button>
-            </Col>
+            </Card.Header>
+            <Card.Body
+              className="align-items-center text-nowrap"
+              style={{ backgroundColor: '#E6E6E6' }}
+            >
+              <Row className="text-md-center">
+                <Col lg className="">
+                  <span style={{ color: '#126DFC' }} className="fw-bold">
+                    Vencimento:
+                  </span>
+                  <span className="fw-bold ms-2">03/10/2024</span>
+                </Col>
+                <Col lg className="">
+                  <span style={{ color: '#126DFC' }} className="fw-bold">
+                    Valor:
+                  </span>
+                  <span className="fw-bold ms-2">R$67,00</span>
+                </Col>
+                <Col lg className="">
+                  <span style={{ color: '#126DFC' }} className="fw-bold">
+                    Adicionais:
+                  </span>
+                  <span className="fw-bold ms-2">R$10,00</span>
+                </Col>
+                <Col lg className="align-items-center">
+                  <span style={{ color: '#126DFC' }} className="fw-bold">
+                    Forma de Pag.:
+                  </span>
+                  <span className="fw-bold ms-2">
+                    Boleto
+                    <a
+                      className="fw-bold text-lg-start fw-bold ms-2"
+                      style={{ color: '#FF3355' }}
+                    >
+                      Mudar
+                    </a>
+                  </span>
+                </Col>
+                <Col lg className="d-flex">
+                  <span style={{ color: '#126DFC' }} className="fw-bold">
+                    Total:
+                  </span>
+                  <span className="fw-bold ms-2">R$77,00</span>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+          <Col md={2} className="d-flex ms-auto">
+            <Button
+              className="fw-bold w-100 p-3 text-end"
+              style={{
+                backgroundColor: '#13c296',
+                color: 'white',
+                border: 'none',
+              }}
+            >
+              Pagar Agora
+            </Button>
           </Col>
         </Row>
         {/* Seção de Histórico de Faturas */}
@@ -110,26 +113,99 @@ export default function InvoicesGroveNFe({}: InvoicesProps) {
                 </tr>
               </thead>
               <tbody>
-                <tr className="d-flex flex-column d-md-table">
+                <tr className="table-row">
+                  <td>
+                    <span className="fw-bolder">02/10/2024</span>
+                  </td>
+                  <td>
+                    <span className="fw-bolder">02/10/2024</span>
+                  </td>
+                  <td>
+                    <span className="fw-bolder">R$77,00</span>
+                  </td>
+                  <td>
+                    <span className="fw-bolder">GroveNFe</span>
+                  </td>
+                  <td>
+                    <Button
+                      className="d-flex ms-auto"
+                      variant="success"
+                      style={{ backgroundColor: '#13c296', border: 'none' }}
+                    >
+                      Pago
+                    </Button>
+                  </td>
+                </tr>
+                <tr className="table-row">
+                  <td>
+                    <span className="fw-bolder">02/10/2024</span>
+                  </td>
+                  <td>
+                    <span className="fw-bolder">02/10/2024</span>
+                  </td>
+                  <td>
+                    <span className="fw-bolder">R$77,00</span>
+                  </td>
+                  <td>
+                    <span className="fw-bolder">GroveNFe</span>
+                  </td>
+                  <td>
+                    <Button
+                      className="d-flex ms-auto"
+                      variant="success"
+                      style={{ backgroundColor: '#13c296', border: 'none' }}
+                    >
+                      Pago
+                    </Button>
+                  </td>
+                </tr>
+                <tr className="d-md-none d-lg-none">
                   <td className="p-0">
-                    <div className="d-lg-none d-flex gap-2 p-2">
+                    <div className=" d-flex gap-2 p-2">
                       <span className="fw-bold">{t('due_date')}:</span>
                       <span>02/10/2024</span>
                     </div>
-                    <div className="d-lg-none d-flex gap-2 p-2">
+                    <div className="d-flex gap-2 p-2">
                       <span className="fw-bold">{t('payment')}:</span>
                       <span>02/10/2024</span>
                     </div>
-                    <div className="d-lg-none d-flex gap-2 p-2">
+                    <div className="d-flex gap-2 p-2">
                       <span className="fw-bold">{t('value')}:</span>
                       <span>R$77,00</span>
                     </div>
-                    <div className="d-lg-none d-flex gap-2 p-2">
+                    <div className="d-flex gap-2 p-2">
                       <span className="fw-bold">{t('services')}:</span>
                       <span>GroveNFe</span>
                     </div>
                     <Button
-                      className="d-lg-none d-flex mb-2 ms-2"
+                      className="d-flex ms-2"
+                      variant="success"
+                      style={{ backgroundColor: '#13c296', border: 'none' }}
+                    >
+                      Pago
+                    </Button>
+                  </td>
+                </tr>
+                <tr className="d-md-none d-lg-none">
+                  <td className="p-0">
+                    <div className=" d-flex gap-2 p-2">
+                      <span className="fw-bold">{t('due_date')}:</span>
+                      <span>02/10/2024</span>
+                    </div>
+                    <div className="d-flex gap-2 p-2">
+                      <span className="fw-bold">{t('payment')}:</span>
+                      <span>02/10/2024</span>
+                    </div>
+                    <div className="d-flex gap-2 p-2">
+                      <span className="fw-bold">{t('value')}:</span>
+                      <span>R$77,00</span>
+                    </div>
+                    <div className="d-flex gap-2 p-2">
+                      <span className="fw-bold">{t('services')}:</span>
+                      <span>GroveNFe</span>
+                    </div>
+                    <Button
+                      className="d-flex ms-2"
                       variant="success"
                       style={{ backgroundColor: '#13c296', border: 'none' }}
                     >
