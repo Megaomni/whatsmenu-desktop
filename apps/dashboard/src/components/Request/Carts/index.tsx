5edc2a3e2843f61b4c3e704963a96e0b160ef1a2
Bruno Santos <brsantos197@gmail.com>
2024-08-27 02:43:00 -0300

    fix: EmitNote

--- apps/dashboard/src/components/Request/Carts/index.tsx
+++ apps/dashboard/src/components/Request/Carts/index.tsx
@@ -104,8 +104,11 @@ export function Carts(data: any) {
   }
 
   const handleEmitNote = async (cart: Cart) => {
+    if(!profile?.options?.integrations?.grovenfe) {
+      return
+    }
     try {
-      const { data } = await groveNfeApi.post(`/v1/fiscalNotes/create/${profile.options.integrations.grovenfe.company_id}`, {
+      await groveNfeApi.post(`/v1/fiscalNotes/create/${profile.options.integrations.grovenfe.company_id}`, {
 
       })
     } catch (error) {
