b07b29771cff0bfddd04f9219427322a46a0f830
Bruno Santos <brsantos197@gmail.com>
2024-09-09 10:38:21 -0300

    chore: Componente para card de integração

--- apps/dashboard/src/pages/dashboard/integrations/index.tsx
+++ apps/dashboard/src/pages/dashboard/integrations/index.tsx
@@ -12,6 +12,7 @@ import { useTranslation } from 'react-i18next'
 import { t } from 'i18next'
 import Image from 'next/image'
 import Link from 'next/link'
+import { IntegrationCard, IntegrationCardContainer } from '@components/IntegrationCard'
 
 export default function Integrations() {
   return (
@@ -40,46 +41,30 @@ export default function Integrations() {
               <p className="fs-6 lh-base mt-2 text-black">
                 {t('online_payment_description')}
               </p>
-              <Row className="mt-4">
-                <Col md={3} className="text-center">
-                  <Link href="/dashboard/profile#forms-payment">
-                    <div
-                      className="d-flex justify-content-center align-items-center w-100 h-50 mt-2 border border-2 border-opacity-50"
-                      style={{ borderRadius: '15px' }}
-                    >
-                      <Card.Body>
-                        <Image
-                          src="/images/AsaasLogo.svg"
-                          alt="Asaas"
-                          width={162}
-                          height={52}
-                        />
-                      </Card.Body>
-                    </div>
-                    <Card.Text className="fs-7 fw-bolder lh-lg text-secondary mb-5 mt-3 text-center">
-                      {t('credit_online')}
-                    </Card.Text>
-                  </Link>
-                </Col>
-                {/* <Col md={3} className="text-center">
-                  <div
-                    className="d-flex justify-content-center align-items-center w-100 h-50 mt-2 border border-2 border-opacity-50"
-                    style={{ borderRadius: '15px' }}
+              <IntegrationCardContainer>
+                <Link href="/dashboard/profile#forms-payment">
+                  <IntegrationCard
+                    legend={t('credit_online')}
                   >
-                    <Card.Body>
-                      <Image
-                        src="/images/GrovePay.svg"
-                        alt="GrovePay"
-                        width={162}
-                        height={52}
-                      />
-                    </Card.Body>
-                  </div>
-                  <Card.Text className="fs-7 fw-bolder lh-lg text-secondary mb-5 mt-3 text-center">
-                    PIX automatizado no seu banco favorito
-                  </Card.Text>
-                </Col> */}
-              </Row>
+                    <Image
+                      src="/images/AsaasLogo.svg"
+                      alt="Asaas"
+                      width={162}
+                      height={52}
+                    />
+                  </IntegrationCard>
+                </Link>
+                {/* <IntegrationCard
+                  legend="PIX automatizado no seu banco favorito"
+                >
+                  <Image
+                    src="/images/GrovePay.svg"
+                    alt="GrovePay"
+                    width={162}
+                    height={52}
+                  />
+                </IntegrationCard> */}
+              </IntegrationCardContainer>
             </Col>
           </Row>
         </CardBody>
@@ -103,26 +88,18 @@ export default function Integrations() {
             <p className="fs-6 lh-base text-black">
               {t('fiscal_note_issuer_description')}
             </p>
-            <Row className="mt-4">
-              <Col md={3} className="text-center">
-                <div
-                  className="d-flex justify-content-center align-items-center w-100 h-50 mt-2 border border-2 border-opacity-50"
-                  style={{ borderRadius: '15px' }}
-                >
-                  <Card.Body>
-                    <Image
-                      src="/images/GroveNFeLogo.svg"
-                      alt="GroveNFe"
-                      width={162}
-                      height={52}
-                    />
-                  </Card.Body>
-                </div>
-                <Card.Text className="fs-7 fw-bolder lh-lg text-secondary mb-5 mt-3 text-center">
-                  {t('grove_nfe_description')}
-                </Card.Text>
-              </Col>
-            </Row>
+            <IntegrationCardContainer>
+              <IntegrationCard
+                legend={t('grove_nfe_description')}
+              >
+                <Image
+                  src="/images/GroveNFeLogo.svg"
+                  alt="GroveNFe"
+                  width={162}
+                  height={52}
+                />
+              </IntegrationCard>
+            </IntegrationCardContainer>
           </Col>
         </Row>
       </Card>
