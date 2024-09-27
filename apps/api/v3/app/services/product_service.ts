0e4ca8351ac2003256073859c15d73775e2b2fd0
Bruno Santos <brsantos197@gmail.com>
2024-09-27 13:40:09 -0300

    fix: Ajuste update produto timeout

--- apps/api/v3/app/services/product_service.ts
+++ apps/api/v3/app/services/product_service.ts
@@ -207,6 +207,7 @@ export class ProductService {
       complements = [...vinculatedComplements, ...newComplements]
 
       await product.related('complements').sync(complements.map((complement) => complement.id))
+      await new Promise<void>((resolve) => setTimeout(resolve, 5000))
       await product.load('complements')
 
       return { product }
