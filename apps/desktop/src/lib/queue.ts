e5f4dad082d84a74caa3f7f6b98556b37ffa86a5
Bruno Santos <brsantos197@gmail.com>
2024-10-01 10:45:23 -0300

    chore: remove old desktop before update

--- apps/desktop/src/lib/queue.ts
+++
@@ -1,6 +0,0 @@
-import Queue from "queue";
-
-export const vouchersToNotifyQueue = new Queue({
-  concurrency: 1,
-  autostart: true,
-});
--- apps/desktop/src/utils/format-currency.ts
+++
@@ -1,6 +0,0 @@
-export const formatCurrency = (value: number, currency = 'BRL') => {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency,
-  }).format(value);
-};
