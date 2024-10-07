22c71e7cc113ae714e59c648053b778d2902d840
Caio César <dev04@whatsmenu.com.br>
2024-10-07 16:34:58 -0300

    feat: Baileys implementado

--- src/windows/tabs/bot-tab.ts
+++ src/windows/tabs/bot-tab.ts
@@ -24,43 +24,71 @@ export const create_bot_tab = () => {
   }
   tab.setVisible(false)
 
-  tab.webContents.on('did-finish-load', () => {
-    whatsAppService.initBot()
-      .then(() => {
-        whatsAppService.bot.on("ready", () => {
-          if (!whatsAppService.firstConection) {
-            tab.webContents.send("onready");
-          } else {
-            whatsAppService.events.on("ready", () => {
-              tab.webContents.send("onready");
-            });
-          }
-        });
+  tab.webContents.on('did-finish-load', async () => {
+    await whatsAppService.connect();
+    whatsAppService.socket.ev.on('connection.update', ({ qr }) => {
+      tab.webContents.send("onqrcode", qr);
+      tab.webContents.send("log", qr);
+    })
+
+    whatsAppService.socket.ev.on('connection.update', ({ connection }) => {
+      switch (connection) {
+        case "close":
+          tab.webContents.send("ondisconnected", "Conexão encerrada");
+          console.log("Conexão encerrada");
+          
+          break;
+        case "open":
+          tab.webContents.send("onready");
+          console.log("Conexão aberta");
+          
+          break; 
+        case "connecting":
+          tab.webContents.send("onloading", { percent: 50, message: 'carregando mensagens' });
+          console.log("Carregando");
+          
+          break;
+      }
+      connection === "open" && tab.webContents.send("onready");
+    })
+
+    whatsAppService.socket.ev
+    // whatsAppService.initBot()
+    //   .then(() => {
+    //     whatsAppService.bot.on("ready", () => {
+    //       if (!whatsAppService.firstConection) {
+    //         tab.webContents.send("onready");
+    //       } else {
+    //         whatsAppService.events.on("ready", () => {
+    //           tab.webContents.send("onready");
+    //         });
+    //       }
+    //     });
     
-        whatsAppService.bot.on("qr", (qr) => {
-          tab.webContents.send("onqrcode", qr);
-          tab.webContents.send("log", qr);
-        });
+    //     whatsAppService.bot.on("qr", (qr) => {
+    //       tab.webContents.send("onqrcode", qr);
+    //       tab.webContents.send("log", qr);
+    //     });
     
-        whatsAppService.bot.on("loading_screen", (percent, message) => {
-          tab.webContents.send("onloading", { percent, message });
-        });
+    //     whatsAppService.bot.on("loading_screen", (percent, message) => {
+    //       tab.webContents.send("onloading", { percent, message });
+    //     });
     
-        whatsAppService.bot.on("disconnected", (reason) => {
-          tab.webContents.send("ondisconnected", reason);
-        });
+    //     whatsAppService.bot.on("disconnected", (reason) => {
+    //       tab.webContents.send("ondisconnected", reason);
+    //     });
     
-        whatsAppService.bot.initialize().catch((err) => {
-          console.error(err);
-          tab.webContents.send("error", err);
-          dialog.showErrorBox("Ops!", err);
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        tab.webContents.send("error", err);
-        dialog.showErrorBox("Ops!", err);
-      })
+    //     whatsAppService.bot.initialize().catch((err) => {
+    //       console.error(err);
+    //       tab.webContents.send("error", err);
+    //       dialog.showErrorBox("Ops!", err);
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //     tab.webContents.send("error", err);
+    //     dialog.showErrorBox("Ops!", err);
+    //   })
   })
 
   return tab
