ccce12e379e15f7f4965e76d4f4e12a8183550e6
Caio César <dev04@whatsmenu.com.br>
2024-10-08 18:14:55 -0300

    feat: continuidade no baileys

--- src/windows/tabs/bot-tab.ts
+++ src/windows/tabs/bot-tab.ts
@@ -1,7 +1,10 @@
-import { dialog, screen } from "electron"
+// import { dialog, screen } from "electron"
 import path from "node:path"
+import { Boom } from "@hapi/boom"
+import fs from "fs"
 import { whatsAppService } from "../../main"
 import { WebTabContentsView } from "../../extends/tab"
+import { DisconnectReason } from "@whiskeysockets/baileys"
 
 export const create_bot_tab = () => {
   
@@ -26,33 +29,48 @@ export const create_bot_tab = () => {
 
   tab.webContents.on('did-finish-load', async () => {
     await whatsAppService.connect();
-    whatsAppService.socket.ev.on('connection.update', ({ qr }) => {
-      tab.webContents.send("onqrcode", qr);
-      tab.webContents.send("log", qr);
-    })
+    // whatsAppService.socket.ev.on('connection.update', ({ qr }) => {
+    //   tab.webContents.send("onqrcode", qr);
+    //   tab.webContents.send("log", qr);
+    // })
 
-    whatsAppService.socket.ev.on('connection.update', ({ connection }) => {
-      switch (connection) {
-        case "close":
-          tab.webContents.send("ondisconnected", "Conexão encerrada");
-          console.log("Conexão encerrada");
-          
-          break;
-        case "open":
-          tab.webContents.send("onready");
-          console.log("Conexão aberta");
-          
-          break; 
+    const connectionUpdate = async (update: any) => {
+      const { connection, lastDisconnect } = update;
+
+      let shouldReconnect = true;
+      if (lastDisconnect) {
+        shouldReconnect = (lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut;
+      }
+      const authFolder = "C:/projects/whatsmenu/apps/desktop/auth";
+
+      if (connection === 'open') {
+        whatsAppService.socket.ev.off('connection.update', connectionUpdate)
+      } else {
+        switch (connection) {
         case "connecting":
           tab.webContents.send("onloading", { percent: 50, message: 'carregando mensagens' });
-          console.log("Carregando");
-          
           break;
+        case "close":
+          if (!shouldReconnect && fs.existsSync(authFolder)) {
+            fs.rmdirSync(authFolder, { recursive: true });
+            tab.webContents.send("ondisconnected", "Conexão encerrada");
+          }
+          whatsAppService.socket.ev.on('connection.update', ({ qr }) => {
+            tab.webContents.send("onqrcode", qr);
+            tab.webContents.send("log", qr);
+          })
+          whatsAppService.socket.ev.off('connection.update', connectionUpdate)
+          break;
+        default:
+          tab.webContents.send("onloading", { percent: 50, message: 'carregando mensagens' });
+          break;
+        }
       }
-      connection === "open" && tab.webContents.send("onready");
-    })
+    }
 
-    whatsAppService.socket.ev
+    whatsAppService.socket.ev.on('connection.update', connectionUpdate);
+
+    // whatsAppService.socket.ev
     // whatsAppService.initBot()
     //   .then(() => {
     //     whatsAppService.bot.on("ready", () => {
