f5ce90c63af8563324ffa31544b37360f0528434
Caio César <dev04@whatsmenu.com.br>
2024-10-11 11:11:11 -0300

    fix: Página atualizando no ato da conexão

--- src/react/bot.tsx
+++ src/react/bot.tsx
@@ -24,11 +24,6 @@ const BotRoot = () => {
   const wsRef = useRef<Ws>(null);
 
   useEffect(() => {
-    console.log("connected on bot file", connected);
-    console.log("profile on bot file", profile);
-    console.log("loading on bot file", loading);
-    
-    
     window.WhatsAppBotApi.onqrcode((_, qr: string) => {
       setLoading((state) => ({ ...state, status: false }));
       setQrcode(qr);
--- src/windows/tabs/bot-tab.ts
+++ src/windows/tabs/bot-tab.ts
@@ -2,10 +2,12 @@
 import path from "node:path"
 import { whatsAppService } from "../../main"
 import { WebTabContentsView } from "../../extends/tab"
-import { ConnectionState } from "@whiskeysockets/baileys"
+import { ConnectionState, DisconnectReason } from "@whiskeysockets/baileys"
+import fs from "fs"
+import { Boom } from "@hapi/boom"
 
 export const create_bot_tab = () => {
-  
+
   const tab = new WebTabContentsView({
     id: "bot",
     webPreferences: {
@@ -33,31 +35,70 @@ export const create_bot_tab = () => {
     // })
 
     const connectionUpdate = async (update: ConnectionState) => {
-      const { connection } = update;
-      const QRCodeGen = whatsAppService.socket.ev.on('connection.update', ({ qr }) => {
+      const { connection, lastDisconnect, qr } = update;
+      if (qr) {
         tab.webContents.send("onqrcode", qr);
         tab.webContents.send("log", qr);
-      })
+        return;
+      }
+      // const QRCodeGen = whatsAppService.socket.ev.on('connection.update', ({ qr }) => {
+      //   tab.webContents.send("onqrcode", qr);
+      //   tab.webContents.send("log", qr);
+      // })
 
-      if (!connection) {
-        QRCodeGen;
-      } else {
-        switch (connection) {
-          case "connecting":
-            tab.webContents.send("onloading", { percent: 50, message: 'carregando mensagens' });
-            break;
-          case "close":
-            tab.webContents.send("ondisconnected", "disconnected");
-            QRCodeGen;
-            break;
-          case "open":
-            tab.webContents.send("onready");
-            break;
-        }
+      const lastDiscReason = (lastDisconnect?.error as Boom)?.output?.statusCode;
+      const storeFile = 'C:/projects/whatsmenu/apps/desktop/baileys_store.json'
+      const authFolder = "C:/projects/whatsmenu/apps/desktop/auth";
+
+
+
+      switch (connection) {
+        case "connecting":
+          tab.webContents.send("onloading", { percent: 50, message: 'carregando mensagens' });
+          break;
+        case "close":
+
+
+          switch (lastDiscReason) {
+            case DisconnectReason.restartRequired || DisconnectReason.timedOut || DisconnectReason.connectionLost || DisconnectReason.connectionClosed:
+              tab.webContents.send("log", `lastdisc - ${JSON.stringify({ lastDiscReason, DisconnectReason }, null, 2)}`);
+              console.log(DisconnectReason);
+              console.log("Reconnecting...");
+              whatsAppService.connect();
+              break;
+            case DisconnectReason.badSession:
+              console.log("Bad session");
+              tab.webContents.send("onqrcode", qr);
+
+              break;
+            case DisconnectReason.connectionReplaced:
+              console.log("Connection replaced");
+              tab.webContents.send("onqrcode", qr);
+
+              break;
+            case DisconnectReason.loggedOut:
+              console.log("Logged out");
+              fs.rmdirSync(authFolder, { recursive: true });
+              fs.rmSync(storeFile);
+              tab.webContents.send("onqrcode", qr);
+
+              break;
+            default:
+              console.log("Unknown reason");
+              tab.webContents.send("onqrcode", qr);
+
+              break;
+          }
+          tab.webContents.send("ondisconnected", "disconnected");
+          break;
+        case "open":
+          tab.webContents.send("onready");
+          break;
       }
     }
 
-    whatsAppService.socket.ev.on('connection.update', connectionUpdate);
+    // whatsAppService.socket.ev.on('connection.update', connectionUpdate);
+    whatsAppService.events.on("connectionUpdate", connectionUpdate);
 
     // whatsAppService.socket.ev
     // whatsAppService.initBot()
@@ -71,20 +112,20 @@ export const create_bot_tab = () => {
     //         });
     //       }
     //     });
-    
+
     //     whatsAppService.bot.on("qr", (qr) => {
     //       tab.webContents.send("onqrcode", qr);
     //       tab.webContents.send("log", qr);
     //     });
-    
+
     //     whatsAppService.bot.on("loading_screen", (percent, message) => {
     //       tab.webContents.send("onloading", { percent, message });
     //     });
-    
+
     //     whatsAppService.bot.on("disconnected", (reason) => {
     //       tab.webContents.send("ondisconnected", reason);
     //     });
-    
+
     //     whatsAppService.bot.initialize().catch((err) => {
     //       console.error(err);
     //       tab.webContents.send("error", err);
