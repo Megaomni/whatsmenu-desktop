ba21fd6715911faea6e04fb58767a067d362dcd9
Caio César <dev04@whatsmenu.com.br>
2024-12-04 18:02:07 -0300

    feat: impressão demo finalizada

--- src/services/printService.ts
+++ src/services/printService.ts
@@ -37,22 +37,86 @@ export const printTest = async (cart: any) => {
     const printBody: PosPrintData[] = cart.itens.map((item: any) => {
         return {
             type: 'text',
-            value: `${item.quantity}x | ${item.name} R$${item.details.value}`,
+            value: `${item.quantity}x | ${item.name} (${item.details.value.toFixed(2)})\n
+            ${item.details.complements.length > 0
+                    ? item.details.complements.map((complement: any) =>
+                        `${complement.itens[0].quantity}x | ${complement.itens[0].name} - ${complement.itens[0].value.toFixed(2)}\n`)
+                    : ''}`,
             style: { fontWeight: "300", fontSize: "15px" }
         }
     });
 
+    const printPayment: PosPrintData[] = [
+        {
+            type: 'text',
+            value: `Sub-total: ${cart.total.toFixed(2)}`,
+            style: { fontWeight: "300", fontSize: "15px" }
+        }, {
+            type: 'text',
+            value: `Taxa de entrega: ${cart.taxDelivery ? `${cart.taxDelivery.toFixed(2)}` : "Grátis"}`,
+            style: { fontWeight: "300", fontSize: "15px" }
+        }, {
+            type: 'text',
+            value: `Total: ${cart.total.toFixed(2)}`,
+            style: { fontWeight: "300", fontSize: "15px" }
+        }, {
+            type: 'text',
+            value: `Pagamento em: ${cart.formsPayment.map((formPayment: any) => `${formPayment.label}`)}`,
+            style: { fontWeight: "300", fontSize: "15px" }
+        }
+    ];
+
+    const printAdress: PosPrintData[] = [
+        {
+            type: 'text',
+            value: `${cart.address.street},`,
+            style: { fontWeight: "300", fontSize: "15px" }
+        },
+        {
+            type: 'text',
+            value: `${cart.address.number} ${cart.address.complement}`,
+            style: { fontWeight: "300", fontSize: "15px" }
+        },
+        {
+            type: 'text',
+            value: `${cart.address.number} - ${cart.address.complement}`,
+            style: { fontWeight: "300", fontSize: "15px" }
+        }
+    ];
+
+    const printFooter: PosPrintData[] = [
+        {
+            type: 'text',
+            value: "**Delivery**",
+            style: { fontWeight: "300", textAlign: 'center', fontSize: "15px" }
+        },
+        {
+            type: 'text',
+            value: "Tecnologia",
+            style: { fontWeight: "300", textAlign: 'center', fontSize: "15px" }
+        },
+        {
+            type: 'text',
+            value: "www.whatsmenu.com.br",
+            style: { fontWeight: "300", textAlign: 'center', fontSize: "15px" }
+        }
+    ];
+
     const fullPrint: PosPrintData[] = [
         ...upperPrint,
-        ...printBody
+        ...printBody,
+        ...printPayment,
+        ...printAdress,
+        ...printFooter
     ];
 
     await PosPrinter.print(fullPrint, {
         printerName: 'POS-80C',
         preview: false,
         silent: true,
-        pageSize: '80mm',
+        pageSize: '76mm',
         margin: '0 0 0 0',
+        boolean: undefined
     }).catch((error) =>
         console.error("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Erro na impressão:", error)
     );
