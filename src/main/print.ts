21be698f913e0461188258456f1dafa091f78914
Caio César <dev04@whatsmenu.com.br>
2025-01-27 12:54:20 -0300

    chore: ajustes finais

--- src/main/print.ts
+++ src/main/print.ts
@@ -2,20 +2,39 @@ import { BrowserWindow } from "electron";
 import isDev from "electron-is-dev";
 import path from "path";
 
+let printWindow: BrowserWindow = null;
+
+/**
+ * Abre uma janela modal para configuração de impressão.
+ *
+ * Se a janela de impressão já existir e não tiver sido destruída, foca nela.
+ * Se a janela estiver minimizada, restaura-a. Caso contrário, cria uma nova
+ * janela de impressão e a abre com o conteúdo da página de configuração
+ * de impressão.
+ *
+ * @returns {BrowserWindow} A janela de impressão criada ou recuperada.
+ */
 export const printModal = () => {
-    const window = new BrowserWindow({
+    if (printWindow && !printWindow.isDestroyed()) {
+        if (printWindow.isMinimized()) printWindow.restore();
+        printWindow.focus();
+        return printWindow;
+    }
+
+    printWindow = new BrowserWindow({
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
         },
     });
-    // isDev && window.webContents.openDevTools();
+
+    // isDev && printWindow.webContents.openDevTools();
 
     if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-        window.webContents.loadURL(
+        printWindow.webContents.loadURL(
             `${MAIN_WINDOW_VITE_DEV_SERVER_URL}/src/views/print-config.html`
         );
     } else {
-        window.webContents.loadFile(
+        printWindow.webContents.loadFile(
             path.join(
                 __dirname,
                 `../renderer/${MAIN_WINDOW_VITE_NAME}/src/views/print-config.html`
@@ -23,5 +42,9 @@ export const printModal = () => {
         );
     }
 
-    return window;
-}
+    printWindow.on('closed', () => {
+        printWindow = null;
+    });
+
+    return printWindow;
+};
