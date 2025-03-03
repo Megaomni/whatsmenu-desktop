3e87198f4dfbcf4f8a7afcca0ebc02d4161179ab
Caio César <dev04@whatsmenu.com.br>
2025-03-03 09:44:42 -0300

    chore: testes realizados

--- src/main/auto-update.ts
+++ src/main/auto-update.ts
@@ -5,12 +5,12 @@ import { tabsWindow } from "../windows/tabs-window";
 import { whatsmenu_api_v3 } from "../lib/axios";
 
 
-let url = "https://whatsmenu-desktop-update-server-beta.vercel.app"
+let url = "https://whatsmenu-desktop-update-server.vercel.app"
 const checkForUpdates = async () => {
   try {
     const profileId = getProfile().id;
     const response = await whatsmenu_api_v3.get(`/user/${profileId}`);
-    const server = !response ? "https://whatsmenu-desktop-update-server.vercel.app" : "https://whatsmenu-desktop-update-server-beta.vercel.app";
+    const server = response.data === 'staging' ? "https://whatsmenu-desktop-update-server-beta.vercel.app" : "https://whatsmenu-desktop-update-server.vercel.app";
     return `${server}/update/${process.platform}/${app.getVersion()}`;
   } catch (error) {
     console.error(error);
@@ -19,52 +19,54 @@ const checkForUpdates = async () => {
 
 checkForUpdates().then((result) => {
   url = result
-})
-const UPDATE_CHECK_INTERVAL = 1000 * 60;
 
-const isMac = process.platform === "darwin";
+  const UPDATE_CHECK_INTERVAL = 1000 * 60;
 
-if (!isMac) {
-  autoUpdater.setFeedURL({ url });
+  const isMac = process.platform === "darwin";
 
-  autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
-    const dialogOpts: Electron.MessageBoxOptions = {
-      type: "info",
-      buttons: ["Reiniciar e atualizar", "Mais tarde"],
-      title: "Atualização disponível!",
-      message: process.platform === "win32" ? releaseNotes : releaseName,
-      detail:
-        "Uma nova versão foi baixada. Reinicie o aplicativo para aplicar as atualizações.",
-    };
+  if (!isMac) {
+    autoUpdater.setFeedURL({ url });
 
-    dialog.showMessageBox(dialogOpts).then((returnValue) => {
-      // mainWindow.removeAllListeners()
-      if (returnValue.response === 0) {
-        tabsWindow.forceCloseWindow();
-        autoUpdater.quitAndInstall();
-      }
+    autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+      const dialogOpts: Electron.MessageBoxOptions = {
+        type: "info",
+        buttons: ["Reiniciar e atualizar", "Mais tarde"],
+        title: "Atualização disponível!",
+        message: process.platform === "win32" ? releaseNotes : releaseName,
+        detail:
+          "Uma nova versão foi baixada. Reinicie o aplicativo para aplicar as atualizações.",
+      };
+
+      dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        // mainWindow.removeAllListeners()
+        if (returnValue.response === 0) {
+          tabsWindow.forceCloseWindow();
+          autoUpdater.quitAndInstall();
+        }
+      });
     });
-  });
 
-  autoUpdater.on("checking-for-update", () => {
-    mainWindow.webContents.send("warn", "Bucando por novas atualizações...");
-  });
+    autoUpdater.on("checking-for-update", () => {
+      mainWindow.webContents.send("warn", "Bucando por novas atualizações...");
+    });
 
-  autoUpdater.on("update-available", () => {
-    mainWindow.webContents.send(
-      "warn",
-      "Nova atualização disponível, baixando...",
-    );
-    clearInterval(updateInterval);
-  });
+    autoUpdater.on("update-available", () => {
+      mainWindow.webContents.send(
+        "warn",
+        "Nova atualização disponível, baixando...",
+      );
+      clearInterval(updateInterval);
+    });
 
-  autoUpdater.on("error", (message) => {
-    mainWindow.webContents.send("error", message);
-    console.error("There was a problem updating the application");
-    console.error(message);
-  });
+    autoUpdater.on("error", (message) => {
+      mainWindow.webContents.send("error", message);
+      console.error("There was a problem updating the application");
+      console.error(message);
+    });
 
-  const updateInterval = setInterval(() => {
-    autoUpdater.checkForUpdates();
-  }, UPDATE_CHECK_INTERVAL);
-}
+    const updateInterval = setInterval(() => {
+      autoUpdater.checkForUpdates();
+    }, UPDATE_CHECK_INTERVAL);
+  }
+})
+
